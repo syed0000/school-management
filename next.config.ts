418f6cb81a8db86c8828d6a71139d84d5a61799d
syed0000 <asadzafar990@gmail.com>
2026-04-15 23:07:33 +0530

    adds redirection to mns.feease.com

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   experimental: {
     serverActions: {
       allowedOrigins: ['localhost:3000', '']
     }
   },
   images: {
-    remotePatterns: [new URL('https://sin1.contabostorage.com/57ac8fdeef3d4c50953d636ac0452829:tenants-app/**')],
+    remotePatterns: [
+      new URL('https://sin1.contabostorage.com/57ac8fdeef3d4c50953d636ac0452829:tenants-app/**')
+    ],
+  },
+
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://mns.feeease.com/:path*',
+        permanent: true,
+      },
+    ];
   },
 };
 
